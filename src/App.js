b6ba7084f9223cb60d158c3c36080b657cabdff0
HassanRamos <ramoshassan42@gmail.com>
2020-04-10 09:33:42 +0300

    added mailchip form

--- src/App.js
+++ src/App.js
@@ -3,39 +3,46 @@ import React from 'react';
 import { Cards, CountryPicker, Chart } from './components';
 import { fetchData } from './api/';
 import styles from './App.module.css';
-
+import MailchimpSubscribe from "react-mailchimp-subscribe"
 import image from './images/image.png';
 
+
 class App extends React.Component {
-  state = {
-    data: {},
-    country: '',
-  }
+    state = {
+        data: {},
+        country: '',
+    }
 
-  async componentDidMount() {
-    const data = await fetchData();
+    async componentDidMount() {
+        const data = await fetchData();
 
-    this.setState({ data });
-  }
+        this.setState({ data });
+    }
 
-  handleCountryChange = async (country) => {
-    const data = await fetchData(country);
+    handleCountryChange = async(country) => {
+        const data = await fetchData(country);
 
-    this.setState({ data, country: country });
-  }
+        this.setState({ data, country: country });
+    }
 
-  render() {
-    const { data, country } = this.state;
+    render() {
+        const { data, country } = this.state;
 
-    return (
-      <div className={styles.container}>
-        <img className={styles.image} src={image} alt="COVID-19" />
-        <Cards data={data} />
-        <CountryPicker handleCountryChange={this.handleCountryChange} />
-        <Chart data={data} country={country} /> 
-      </div>
-    );
-  }
+        return (
+
+          <div className = { styles.container } >
+          <h5 style={{color:'#152F8D'}}> Subscribe to our email newsletter today to receive  latest news on Covid-19 </h5>
+          <MailchimpSubscribe url = { process.env.REACT_APP_MAILCHIMP_URL }/>
+          <img className = { styles.image }src = { image } alt = "COVID-19"/>
+            <Cards data = { data }/>
+             <CountryPicker handleCountryChange = { this.handleCountryChange }/>
+             <Chart data = { data } country = { country }/>
+             </div>
+
+
+
+        );
+    }
 }
 
 export default App;
